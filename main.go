8b6682f4df0e652731fdff386978135e6ed658f1
Tommi <tommi-lew@users.noreply.github.com>
2022-02-09 13:10:02 +0800

    feat: add heartbeat endpoint

--- main.go
+++ main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
 func main() {
 	mux := http.NewServeMux()
 
+	mux.HandleFunc("/api/heartbeat", heartbeat)
+
 	http.ListenAndServe("localhost:8000", mux)
+}
+
+func heartbeat(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Pong")
 }
